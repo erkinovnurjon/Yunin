b50bb6a81ccb13c2837e96ac288f70289d17ffc7
Nurjon <123224958+erkinovnurjon@users.noreply.github.com>
2024-10-28 11:52:33 +0500

    sas

--- src/components/custom/sidebar/menu.ts
+++ src/components/custom/sidebar/menu.ts
@@ -1,11 +1,20 @@
 import { type Menu } from "@/modules/basics";
+
+import {
+  Bell,
+  CircleUser,
+  Home,
+  Package,
+  Package2,
+  Search,
+} from "lucide-vue-next";
 export const menus: Menu[] = [
   {
     name: "Products",
     to: "/products",
     childrenVisible: false,
     children: [],
-    icon: "",
+    icon: "Home",
   },
   {
     name: "Overview",
--- src/components/custom/sidebar/sidebar.vue
+++ src/components/custom/sidebar/sidebar.vue
@@ -1,14 +1,14 @@
-<template>
+<!-- <template>
   <div class="w-full flex items-center">
     <navbar class="shadow-md w-full py-1 flex items-center justify-between">
-      <div class="flex items-center justify-start">
+      <div class="flex relative items-center justify-start">
         <div class="flex items-center">
           <img :src="Logo" alt="Yunin Logo" class="h-8 w-8 mr-2" />
           <h1 class="text-3xl font-bold">Yunin</h1>
         </div>
-        <Button
+        <div
           @click="toggleSidebar"
-          class="text-gray-800 material-icons hover:bg-slate-400 animate-in transition-all bg-transparent focus:outline-none"
+          class="text-gray-800 absolute material-icons left-60 top-10 bg-slate-400 animate-in transition-all bg-transparent focus:outline-none"
         >
           <Icon
             icon="radix-icons:moon"
@@ -21,9 +21,9 @@
           <span class="material-icons" style="font-size: 28px">
             {{ isSidebarOpen ? "chevron_left" : "chevron_right" }}
           </span>
-        </Button>
+        </div>
       </div>
-      <div>Bankai</div>
+      <div><Theme /></div>
     </navbar>
   </div>
   <div class="flex">
@@ -76,10 +76,10 @@
 
 <script setup lang="ts">
 import { ref } from "vue";
-import Button from "@/components/ui/button/Button.vue";
 import Logo from "@/assets/images/logo/Logo.jpg";
 import { menus } from "./menu.ts";
 import { type Menu } from "@/modules/basics";
+import Theme from "@/components/theme/theme.vue";
 
 const routes = ref<Menu[]>(menus);
 const isSidebarOpen = ref<boolean>(true);
@@ -100,4 +100,131 @@ const toggleChildren = (name: string) => {
 .transition-all {
   transition-property: all;
 }
-</style>
+</style> -->
+
+<script setup lang="ts">
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Input } from "@/components/ui/input";
+import Theme from "@/components/theme/theme.vue";
+
+import { type Menu } from "@/modules/basics";
+import { menus } from "./menu.ts";
+import { ref } from "vue";
+
+const routes = ref<Menu[]>(menus);
+
+const toggleChildren = (name: string) => {
+  const route = routes.value.find((item: any) => item.name === name);
+  if (route) {
+    route.childrenVisible = !route.childrenVisible;
+  }
+};
+</script>
+
+<template>
+  <div
+    class="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]"
+  >
+    <div class="hidden border-r bg-muted/40 md:block">
+      <div class="flex h-full max-h-screen flex-col gap-2">
+        <div class="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
+          <a href="/" class="flex items-center gap-2 font-semibold">
+            <Package2 class="h-6 w-6" />
+            <span class="">Yunin</span>
+          </a>
+          <Button variant="outline" size="icon" class="ml-auto h-8 w-8">
+            <Bell class="h-4 w-4" />
+            <span class="sr-only">Toggle notifications</span>
+          </Button>
+        </div>
+        <div class="flex-1">
+          <nav
+            v-for="item in routes"
+            :key="item.name"
+            class="grid items-start px-2 text-sm font-medium lg:px-4"
+          >
+            <router-link
+              :to="item.to"
+              @click.prevent="toggleChildren(item.name)"
+              class="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
+            >
+              <Home class="h-4 w-4" />
+              {{ item.name }}
+            </router-link>
+          </nav>
+        </div>
+        <div class="mt-auto p-4">
+          <Card>
+            <CardHeader class="p-2 pt-0 md:p-4">
+              <CardTitle>Upgrade to Pro</CardTitle>
+              <CardDescription>
+                Unlock all features and get unlimited access to our support
+                team.
+              </CardDescription>
+            </CardHeader>
+            <CardContent class="p-2 pt-0 md:p-4 md:pt-0">
+              <Button size="sm" class="w-full"> Upgrade </Button>
+            </CardContent>
+          </Card>
+        </div>
+      </div>
+    </div>
+    <div class="flex flex-col">
+      <header
+        class="flex h-14 items-center gap-4 border-b bg-muted/40 px-4 lg:h-[60px] lg:px-6"
+      >
+        <div class="w-full flex-1">
+          <form>
+            <div class="relative">
+              <Search
+                class="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground"
+              />
+              <Input
+                type="search"
+                placeholder="Search products..."
+                class="w-full appearance-none bg-background pl-8 shadow-none md:w-2/3 lg:w-1/3"
+              />
+            </div>
+          </form>
+        </div>
+        <div>
+          <Theme />
+          <DropdownMenu>
+            <DropdownMenuTrigger as-child>
+              <Button variant="secondary" size="icon" class="rounded-full">
+                <CircleUser class="h-5 w-5" />
+                <span class="sr-only">Toggle user menu</span>
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+              <DropdownMenuLabel>My Account</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>Settings</DropdownMenuItem>
+              <DropdownMenuItem>Support</DropdownMenuItem>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>Logout</DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </div>
+      </header>
+      <main class="lex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
+        <slot></slot>
+      </main>
+    </div>
+  </div>
+</template>
