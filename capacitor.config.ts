4d282d6ef38a1e3de2d30f616f113c6948281628
DoniyorbekErkinov <erkinovdoniyor@gmail.com>
2024-11-12 17:12:34 +0500

    ionic capicat added

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'yunin.uz',
+  appName: 'Yunin',
+  webDir: 'dist'
+};
+
+export default config;
